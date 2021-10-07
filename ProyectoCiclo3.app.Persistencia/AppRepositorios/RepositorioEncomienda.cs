e8b03c40f43a7bf2dadac02fed8540b4467ddb54
luifercat <luifercat@gmail.com>
2021-10-07 09:52:43 -0500

    20211007-CRUD con BD us-en

--- ProyectoCiclo3.app.Persistencia/AppRepositorios/RepositorioEncomienda.cs
+++ ProyectoCiclo3.app.Persistencia/AppRepositorios/RepositorioEncomienda.cs
@@ -7,56 +7,101 @@ namespace ProyectoCiclo3.App.Persistencia.AppRepositorios
 {
     public class RepositorioEncomienda
     {
-        List<Encomienda> encomiendas;
+        //List<Encomienda> encomiendas;
+
+        //sgte linea permite conexion a BD
+        private readonly AppContext _appContext = new AppContext();
  
-    public RepositorioEncomienda()
-        {
-            encomiendas= new List<Encomienda>()
-            {
-                new Encomienda{id=1,descripcion="Paquete",peso= 100,tipo= "pequeño",presentacion= "aaaaaaa"},
-                new Encomienda{id=2,descripcion="Sobre",peso= 200,tipo= "grande",presentacion= "bbbbbbbb"},
-                new Encomienda{id=3,descripcion="Documentos",peso= 300,tipo= "mediano",presentacion= "cccccccc"} 
+    // se elimina este metodo constructor que se usaba antes de la conexion a BD
+    // public RepositorioEncomienda()
+    //     {
+    //         encomiendas= new List<Encomienda>()
+    //         {
+    //             new Encomienda{id=1,descripcion="Paquete",peso= 100,tipo= "pequeño",presentacion= "aaaaaaa"},
+    //             new Encomienda{id=2,descripcion="Sobre",peso= 200,tipo= "grande",presentacion= "bbbbbbbb"},
+    //             new Encomienda{id=3,descripcion="Documentos",peso= 300,tipo= "mediano",presentacion= "cccccccc"} 
+     //         };
+    //     }
  
-            };
-        }
+        // public IEnumerable<Encomienda> GetAll()     usado antes de conexioon a BD
+        // {
+        //     return encomiendas;
+        // }
  
+        // se hace referencia a dbset de la BD, se trae todas las encomiendas de la BD
         public IEnumerable<Encomienda> GetAll()
         {
-            return encomiendas;
+           return _appContext.Encomiendas;    //hace referencia al dbset de appcontext 
         }
- 
-        public Encomienda GetEncomiendaWithId(int id){
-            return encomiendas.SingleOrDefault(b => b.id == id);
+
+
+        // public Encomienda GetEncomiendaWithId(int id){       usado antes de conexion a BD
+        //     return encomiendas.SingleOrDefault(b => b.id == id);
+        // }
+
+
+        public Encomienda GetEncomiendaWithId(int id){   //usado para BD, trae Id de la encomienda
+            return _appContext.Encomiendas.Find(id);
         }
+        // public Encomienda Create(Encomienda newEncomienda)      usado antes de conexion a BD
+        // {
+        //    if(encomiendas.Count > 0){
+        //    newEncomienda.id=encomiendas.Max(r => r.id) +1; 
+        //     }else{
+        //        newEncomienda.id = 1; 
+        //     }
+        //    encomiendas.Add(newEncomienda);
+        //    return newEncomienda;
+        // }
+
+        //siguente metodo agrega encomienda y la devuelve
         public Encomienda Create(Encomienda newEncomienda)
         {
-           if(encomiendas.Count > 0){
-           newEncomienda.id=encomiendas.Max(r => r.id) +1; 
-            }else{
-               newEncomienda.id = 1; 
-            }
-           encomiendas.Add(newEncomienda);
-           return newEncomienda;
-        }
+            var addEncomienda = _appContext.Encomiendas.Add(newEncomienda);
+            _appContext.SaveChanges();
+            return addEncomienda.Entity;
+        }  
+        // public Encomienda Update(Encomienda newEncomienda){
+        //     var encomienda= encomiendas.SingleOrDefault(b => b.id == newEncomienda.id);
+        //     if(encomienda != null){
+        //         encomienda.descripcion = newEncomienda.descripcion;
+        //         encomienda.peso = newEncomienda.peso;
+        //         encomienda.tipo = newEncomienda.tipo;
+        //         encomienda.presentacion = newEncomienda.presentacion;
+        //         // encomienda.ciudad = newEncomienda.ciudad;
+        //     }
+        // return encomienda;
+        // }
+
         public Encomienda Update(Encomienda newEncomienda){
-            var encomienda= encomiendas.SingleOrDefault(b => b.id == newEncomienda.id);
-            if(encomienda != null){
-                encomienda.descripcion = newEncomienda.descripcion;
-                encomienda.peso = newEncomienda.peso;
-                encomienda.tipo = newEncomienda.tipo;
-                encomienda.presentacion = newEncomienda.presentacion;
-                // encomienda.ciudad = newEncomienda.ciudad;
+            var enco = _appContext.Encomiendas.Find(newEncomienda.id);
+
+            if(enco != null){
+                enco.descripcion = newEncomienda.descripcion;
+                enco.peso = newEncomienda.peso;
+                enco.tipo = newEncomienda.tipo;
+                enco.presentacion = newEncomienda.presentacion;
+                //enco.ciudad = newEncomienda.ciudad;
+                //Guardar en base de datos
+                 _appContext.SaveChanges();
             }
-        return encomienda;
+            return enco;
         }
 
-
+        // public void Delete(int id)   antes de conexion a BD
+        // {
+        // var enco= encomiendas.SingleOrDefault(b => b.id == id);
+        // encomiendas.Remove(enco);
+        // return;
+        //}
 
         public void Delete(int id)
         {
-        var enco= encomiendas.SingleOrDefault(b => b.id == id);
-        encomiendas.Remove(enco);
-        return;
+            var enco = _appContext.Encomiendas.Find(id);
+            if (enco == null)
+                return;
+            _appContext.Encomiendas.Remove(enco);
+            _appContext.SaveChanges();
         }
     }
 }
--- ProyectoCiclo3.app.Persistencia/AppRepositorios/RepositorioUsuario.cs
+++ ProyectoCiclo3.app.Persistencia/AppRepositorios/RepositorioUsuario.cs
@@ -7,56 +7,101 @@ namespace ProyectoCiclo3.App.Persistencia.AppRepositorios
 {
     public class RepositorioUsuario
     {
-        List<Usuario> usuarios;
+        //List<Usuario> usuarios;
+        private readonly AppContext _appContext = new AppContext();
  
-    public RepositorioUsuario()
-        {
-            usuarios= new List<Usuario>()
-            {
-                new Usuario{id=1,nombre="Mario",apellidos= "Sánchez",direccion= "Carrera 27",telefono= "8945412121"},
-                new Usuario{id=2,nombre="Camila",apellidos= "Rodriguez",direccion= "Calle 96",telefono= "895656562"},
-                new Usuario{id=3,nombre="Maria Carolina",apellidos= "Perez",direccion= "Avenida Sur con 14",telefono= "12145454"} 
+    // public RepositorioUsuario()       se elimina ya no se necesita, se va a trar la inf de la bd
+    //     {
+    //         usuarios= new List<Usuario>()
+    //         {
+    //             new Usuario{id=1,nombre="Mario",apellidos= "Sánchez",direccion= "Carrera 27",telefono= "8945412121"},
+    //             new Usuario{id=2,nombre="Camila",apellidos= "Rodriguez",direccion= "Calle 96",telefono= "895656562"},
+    //             new Usuario{id=3,nombre="Maria Carolina",apellidos= "Perez",direccion= "Avenida Sur con 14",telefono= "12145454"} 
  
-            };
-        }
+    //         };
+    //     }
  
+        // public IEnumerable<Usuario> GetAll() antes de conexion a BD
+        // {
+        //     return usuarios;
+        // }
+
+        // GetAll para la BD -  trae todos los usuario de la bd
         public IEnumerable<Usuario> GetAll()
         {
-            return usuarios;
+           return _appContext.Usuarios;
         }
  
-        public Usuario GetUsuarioWithId(int id){
-            return usuarios.SingleOrDefault(b => b.id == id);
-        }
+        // public Usuario GetUsuarioWithId(int id){     ANTES DE CONEXION A BD
+        //     return usuarios.SingleOrDefault(b => b.id == id);
+        // }
 
+        // GetUsuarioWithId para BD 
+        public Usuario GetUsuarioWithId(int id){
+            return _appContext.Usuarios.Find(id);
+        }
+        // public Usuario Create(Usuario newUsuario)    Antes de conectar a BD
+        // {
+        //    if(usuarios.Count > 0){
+        //    newUsuario.id=usuarios.Max(r => r.id) +1; 
+        //     }else{
+        //        newUsuario.id = 1; 
+        //     }
+        //    usuarios.Add(newUsuario);
+        //    return newUsuario;
+        // }
+
+        // metodo create con BD
         public Usuario Create(Usuario newUsuario)
         {
-           if(usuarios.Count > 0){
-           newUsuario.id=usuarios.Max(r => r.id) +1; 
-            }else{
-               newUsuario.id = 1; 
-            }
-           usuarios.Add(newUsuario);
-           return newUsuario;
+            var addUsuario = _appContext.Usuarios.Add(newUsuario);
+            _appContext.SaveChanges();
+            return addUsuario.Entity;      //retorna la entidad usuario que se acaba de crear
         }
+
+        // public Usuario Update(Usuario newUsuario){     ANTES DE CONEXION A BD
+        //     var usuario= usuarios.SingleOrDefault(b => b.id == newUsuario.id);
+        //     if(usuario != null){
+        //         usuario.nombre = newUsuario.nombre;
+        //         usuario.apellidos = newUsuario.apellidos;
+        //         usuario.direccion = newUsuario.direccion;
+        //         usuario.telefono = newUsuario.telefono;
+        //         // usuario.ciudad = newUsuario.ciudad;
+        //     }
+        // return usuario;
+        // }
+
+        // Update con BD
         public Usuario Update(Usuario newUsuario){
-            var usuario= usuarios.SingleOrDefault(b => b.id == newUsuario.id);
-            if(usuario != null){
-                usuario.nombre = newUsuario.nombre;
-                usuario.apellidos = newUsuario.apellidos;
-                usuario.direccion = newUsuario.direccion;
-                usuario.telefono = newUsuario.telefono;
-                // usuario.ciudad = newUsuario.ciudad;
+            var user = _appContext.Usuarios.Find(newUsuario.id);
+
+            if(user != null){
+                user.nombre = newUsuario.nombre;
+                user.apellidos = newUsuario.apellidos;
+                user.direccion = newUsuario.direccion;
+                user.telefono = newUsuario.telefono;
+                // user.ciudad = newUsuario.ciudad;
+                //Guardar en base de datos
+                 _appContext.SaveChanges();
             }
-        return usuario;
+            return user;
         }
 
+        // public void Delete(int id)
+        // {
+        // var user= usuarios.SingleOrDefault(b => b.id == id);
+        // usuarios.Remove(user);
+        // return;
+        //}
 
+        // metodo delete con BD
         public void Delete(int id)
         {
-        var user= usuarios.SingleOrDefault(b => b.id == id);
-        usuarios.Remove(user);
-        return;
+        var user = _appContext.Usuarios.Find(id);
+        if (user == null)
+            return;
+        _appContext.Usuarios.Remove(user);
+        _appContext.SaveChanges();
         }
     }
 }
